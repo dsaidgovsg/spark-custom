70e1555c8ae3970c796a98151fa1adec7aa5e74d
Chen Weiguang <chen.weiguang@gmail.com>
2019-08-29 11:30:32 +0800

    Fix tag to include Python version

--- push-images.sh
+++ push-images.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 IMAGE_NAME=${IMAGE_NAME:-spark-custom}
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}_${HIVE_TAG_SUFFIX}${PYSPARK_TAG_SUFFIX}_${DIST}
